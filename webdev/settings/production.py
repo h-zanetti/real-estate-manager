48c36a2fbd2b803a64e972606ebcc17d193ea476
agah <henriaguiar99@gmail.com>
2021-06-16 18:35:39 -0300

    Adicionado arquivo settings para produção

---
+++ webdev/settings/production.py
@@ -0,0 +1,24 @@
+from webdev.settings.base import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['casas-de-hermes.agahsolutions.com']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+    }
+}
