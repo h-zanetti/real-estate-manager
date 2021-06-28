d540a6d67d7a1a8afa78b4ce27a764dd93d9b622
agah <henriaguiar99@gmail.com>
2021-06-28 16:12:36 -0300

    Alterado allowed hosts em settings.production

--- webdev/settings/production.py
+++ webdev/settings/production.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['casas-de-hermes.agahsolutions.com']
+ALLOWED_HOSTS = ['casasdehermes.com.br']
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
