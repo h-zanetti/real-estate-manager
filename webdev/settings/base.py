37249ff6950ef3945a48fc3455efbef46f2866b8
agah <henriaguiar99@gmail.com>
2021-06-30 16:54:43 -0300

    Separadas variáveis de arquivos estáticos entre ambientes de produção e desenvolvimento

--- webdev/settings/base.py
+++ webdev/settings/base.py
@@ -102,16 +102,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = 'hermes/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
-
+# Media files
 MEDIA_URL = 'hermes/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
--- webdev/settings/development.py
+++ webdev/settings/development.py
@@ -22,3 +22,11 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = 'hermes/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
